48a9df2becae67df4d400d022f55c51c91dbbced
Alikhano <nannette121@gmail.com>
2018-09-08 13:44:11 +0300

    attempt to delete .project/.settings from git + homepage quick fix

--- cyberlife/src/main/java/ru/alikhano/cyberlife/controller/HomeController.java
+++ cyberlife/src/main/java/ru/alikhano/cyberlife/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	// test controller, this method will actually redirect to a home page
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
